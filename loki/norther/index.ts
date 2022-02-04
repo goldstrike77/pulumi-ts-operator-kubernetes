248d082b4f0b180412cd5b670071a34b6d46cee4
oper <oper@clu-p-cen-oss01.node.dc01.local>
2022-02-04 09:42:33 +0800

    Initial commit

---
+++ loki/norther/index.ts
@@ -0,0 +1,60 @@
+import * as k8s from "@pulumi/kubernetes";
+import { FileAsset } from "@pulumi/pulumi/asset";
+
+const deploy_spec = [
+    {
+        namespace: {
+            metadata: {
+                name: "logging",
+                annotations: {},
+                labels: {}
+            },
+            spec: {}
+        },
+        helm: [
+            {
+                namespace: "logging",
+                name: "loki",
+                chart: "../../_chart/loki-distributed-0.42.2.tgz",
+                // repository: "https://grafana.github.io/helm-charts",
+                repository: "", // Must be empty string if local chart.
+                version: "0.42.2",
+                values: "./loki-distributed.yaml"
+            }
+        ]
+    }
+]
+
+for (var i in deploy_spec) {
+    // Create Kubernetes Namespace.
+    const namespace = new k8s.core.v1.Namespace(deploy_spec[i].namespace.metadata.name, {
+        metadata: deploy_spec[i].namespace.metadata,
+        spec: deploy_spec[i].namespace.spec
+    });
+    // Create Release Resource.
+    for (var helm_index in deploy_spec[i].helm) {
+        if (deploy_spec[i].helm[helm_index].repository === "") {
+            const release = new k8s.helm.v3.Release(deploy_spec[i].helm[helm_index].name, {
+                namespace: deploy_spec[i].helm[helm_index].namespace,
+                name: deploy_spec[i].helm[helm_index].name,
+                chart: deploy_spec[i].helm[helm_index].chart,
+                version: deploy_spec[i].helm[helm_index].version,
+                valueYamlFiles: [new FileAsset(deploy_spec[i].helm[helm_index].values)],
+                skipAwait: true,
+            }, { dependsOn: [namespace] });
+        }
+        else {
+            const release = new k8s.helm.v3.Release(deploy_spec[i].helm[helm_index].name, {
+                namespace: deploy_spec[i].helm[helm_index].namespace,
+                name: deploy_spec[i].helm[helm_index].name,
+                chart: deploy_spec[i].helm[helm_index].chart,
+                version: deploy_spec[i].helm[helm_index].version,
+                valueYamlFiles: [new FileAsset(deploy_spec[i].helm[helm_index].values)],
+                skipAwait: true,
+                repositoryOpts: {
+                    repo: deploy_spec[i].helm[helm_index].repository,
+                },
+            }, { dependsOn: [namespace] });
+        }
+    }
+}
