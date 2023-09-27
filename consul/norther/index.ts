4d4c2034b22f3ef57f7b0a374bc4b6eaf43b14fd
suzhetao <suzhetao@gmail.com>
2023-09-27 13:27:41 +0800

    modified:   consul/norther/index.ts

--- consul/norther/index.ts
+++ consul/norther/index.ts
@@ -44,13 +44,10 @@ const deploy_spec = [
                     limits: { cpu: "200m", memory: "128Mi" },
                     requests: { cpu: "200m", memory: "128Mi" }
                 },
-                service: {
-                    ports: {
-                        http: "8500"
-                    },
-                    type: "LoadBalancer",
-                    loadBalancerSourceRanges: ["192.168.0.0/24"],
-                    annotations: { "metallb.universe.tf/allow-shared-ip": "shared" }
+                ingress: {
+                    enabled: true,
+                    hostname: "consul.example.com",
+                    ingressClassName: "nginx"
                 },
                 persistence: {
                     enabled: true,
