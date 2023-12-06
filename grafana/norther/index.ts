4fc0eb998cf7fa40f5fe0aeb2d8d4517b020f936
suzhetao <suzhetao@gmail.com>
2023-12-06 18:22:40 +0800

    modified:   grafana/norther/Pulumi.norther-grafana.yaml 	modified:   grafana/norther/dashboards/platform/VMware_vSphere_Overview.json 	modified:   grafana/norther/index.ts 	modified:   vmware-exporter/norther/index.ts

--- grafana/norther/index.ts
+++ grafana/norther/index.ts
@@ -1,10 +1,19 @@
-import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
+import * as k8s_module from '../../../module/pulumi-ts-module-kubernetes';
 import * as fs from 'fs';
 
 let config = new pulumi.Config();
 
-const deploy_spec = [
+const podlabels = {
+    customer: "demo",
+    environment: "dev",
+    project: "Visualization",
+    group: "grafana",
+    datacenter: "dc01",
+    domain: "local"
+}
+
+const resources = [
     {
         namespace: {
             metadata: {
@@ -116,201 +125,174 @@ const deploy_spec = [
                 }
             }
         ],
-        helm: {
-            namespace: "visualization",
-            name: "grafana",
-            chart: "grafana",
-            repository: "https://grafana.github.io/helm-charts",
-            version: "6.57.4",
-            values: {
-                replicas: 1,
-                deploymentStrategy: {
-                    type: "RollingUpdate",
-                    rollingUpdate: {
-                        maxSurge: 0,
-                        maxUnavailable: 1
-                    }
+        release: [
+            {
+                namespace: "visualization",
+                name: "grafana",
+                chart: "grafana",
+                repositoryOpts: {
+                    repo: "https://grafana.github.io/helm-charts"
                 },
-                podLabels: { customer: "demo", environment: "dev", project: "Visualization", group: "grafana", datacenter: "dc01", domain: "local" },
-                serviceMonitor: {
-                    enabled: true,
-                    relabelings: [
-                        { sourceLabels: ["__meta_kubernetes_pod_name"], separator: ";", regex: "^(.*)$", targetLabel: "instance", replacement: "$1", action: "replace" },
-                        { sourceLabels: ["__meta_kubernetes_pod_label_customer"], targetLabel: "customer" },
-                        { sourceLabels: ["__meta_kubernetes_pod_label_environment"], targetLabel: "environment" },
-                        { sourceLabels: ["__meta_kubernetes_pod_label_project"], targetLabel: "project" },
-                        { sourceLabels: ["__meta_kubernetes_pod_label_group"], targetLabel: "group" },
-                        { sourceLabels: ["__meta_kubernetes_pod_label_datacenter"], targetLabel: "datacenter" },
-                        { sourceLabels: ["__meta_kubernetes_pod_label_domain"], targetLabel: "domain" }
-                    ]
-                },
-                ingress: {
-                    enabled: true,
-                    ingressClassName: "nginx",
-                    annotations: {
-                        "nginx.ingress.kubernetes.io/rewrite-target": "/$1",
-                        "nginx.ingress.kubernetes.io/use-regex": "true"
+                version: "6.57.4",
+                values: {
+                    replicas: 1,
+                    deploymentStrategy: {
+                        type: "RollingUpdate",
+                        rollingUpdate: {
+                            maxSurge: 0,
+                            maxUnavailable: 1
+                        }
                     },
-                    path: "/grafana/?(.*)",
-                    hosts: ["norther.example.com"],
-                },
-                resources: {
-                    limits: { cpu: "200m", memory: "384Mi" },
-                    requests: { cpu: "200m", memory: "384Mi" }
-                },
-                persistence: {
-                    enabled: true,
-                    storageClassName: "longhorn",
-                    size: "7Gi"
-                },
-                initChownData: {
-                    enabled: true,
-                    resources: {
-                        limits: { cpu: "100m", memory: "128Mi" },
-                        requests: { cpu: "100m", memory: "128Mi" }
-                    }
-                },
-                adminUser: "admin",
-                adminPassword: config.require("adminPassword"),
-                plugins: [
-                    "grafana-piechart-panel",
-                    "camptocamp-prometheus-alertmanager-datasource",
-                    "grafana-oncall-app"
-                ],
-                datasources: {
-                    "datasources.yaml": {
-                        apiVersion: 1,
-                        datasources: [
-                            {
-                                name: "DS_TEMPO",
-                                type: "tempo",
-                                access: "proxy",
-                                url: "http://tempo-query-frontend.tracing.svc.cluster.local:3100",
-                                version: 1
-                            },
-                            {
-                                name: "DS_LOKI",
-                                type: "loki",
-                                access: "proxy",
-                                url: "http://loki-query-frontend.logging.svc.cluster.local:3100",
-                                version: 1,
-                                jsonData: {
-                                    maxLines: 5000
-                                },
-                                derivedFields: {
-                                    datasourceUid: "",
-                                    matcherRegex: "(?:traceID|trace_id)=(\\w+)",
-                                    name: "TraceID",
-                                    url: "${__value.raw}"
-                                }
-                            },
-                            {
-                                name: "DS_PROMETHEUS",
-                                type: "prometheus",
-                                access: "proxy",
-                                url: "http://thanos-query-frontend.monitoring.svc.cluster.local:9090",
-                                version: 1
-                            },
-                            {
-                                name: "DS_ALERTMANAGER",
-                                type: "camptocamp-prometheus-alertmanager-datasource",
-                                access: "proxy",
-                                url: "http://kube-prometheus-stack-alertmanager.monitoring.svc.cluster.local:9093",
-                                version: 1,
-                                jsonData: {
-                                    severity_critical: "p1",
-                                    severity_high: "p2",
-                                    severity_warning: "p3",
-                                    severity_info: "p4"
-                                }
-                            }
-                        ]
-                    }
-                },
-                "grafana.ini": {
-                    "auth.azuread": {
-                        name: "Azure AAD",
+                    podLabels: podlabels,
+                    serviceMonitor: {
                         enabled: true,
-                        allow_sign_up: true,
-                        client_id: "707fb37f-06f3-4fad-842b-59f95b03a9cb",
-                        client_secret: config.require("client_secret"),
-                        auth_url: "https://login.microsoftonline.com/1028c8b9-5db4-4ade-bd31-524340b7cc0d/oauth2/v2.0/authorize",
-                        token_url: "https://login.microsoftonline.com/1028c8b9-5db4-4ade-bd31-524340b7cc0d/oauth2/v2.0/token",
-                        role_attribute_strict: false
+                        relabelings: [
+                            { sourceLabels: ["__meta_kubernetes_pod_name"], separator: ";", regex: "^(.*)$", targetLabel: "instance", replacement: "$1", action: "replace" },
+                            { sourceLabels: ["__meta_kubernetes_pod_label_customer"], targetLabel: "customer" },
+                            { sourceLabels: ["__meta_kubernetes_pod_label_environment"], targetLabel: "environment" },
+                            { sourceLabels: ["__meta_kubernetes_pod_label_project"], targetLabel: "project" },
+                            { sourceLabels: ["__meta_kubernetes_pod_label_group"], targetLabel: "group" },
+                            { sourceLabels: ["__meta_kubernetes_pod_label_datacenter"], targetLabel: "datacenter" },
+                            { sourceLabels: ["__meta_kubernetes_pod_label_domain"], targetLabel: "domain" }
+                        ]
                     },
-                    server: {
-                        root_url: "https://norther.example.com/grafana",
+                    ingress: {
+                        enabled: true,
+                        ingressClassName: "nginx",
+                        annotations: {
+                            "nginx.ingress.kubernetes.io/rewrite-target": "/$1",
+                            "nginx.ingress.kubernetes.io/use-regex": "true"
+                        },
+                        path: "/grafana/?(.*)",
+                        hosts: ["norther.example.com"],
                     },
-                    paths: {
-                        data: "/var/lib/grafana/",
-                        logs: "/var/log/grafana",
-                        plugins: "/var/lib/grafana/plugins",
-                        provisioning: "/etc/grafana/provisioning",
-                    },
-                    dataproxy: {
-                        timeout: "60",
-                        keep_alive_seconds: "60"
-                    },
-                    analytics: {
-                        check_for_updates: false,
-                        reporting_enabled: false
-                    },
-                    log: { mode: "console", level: "warn" },
-                    grafana_net: { url: "https://grafana.net" },
-                    user: {
-                        default_theme: "dark",
-                        home_page: ""
-                    },
-                    // tracing: { type: "jaeger" },
-                    // "tracing.jaeger": {
-                    //     address: "tempo-distributor.tracing.svc.cluster.local:6831",
-                    //     zipkin_propagation: true
-                    // }
-                },
-                sidecar: {
                     resources: {
-                        limits: { cpu: "50m", memory: "128Mi" },
-                        requests: { cpu: "50m", memory: "128Mi" }
+                        limits: { cpu: "200m", memory: "384Mi" },
+                        requests: { cpu: "200m", memory: "384Mi" }
                     },
-                    dashboards: { enabled: true, label: "grafana_dashboard" }
+                    persistence: {
+                        enabled: true,
+                        storageClassName: "vsphere-san-sc",
+                        size: "7Gi"
+                    },
+                    initChownData: {
+                        enabled: true,
+                        resources: {
+                            limits: { cpu: "100m", memory: "128Mi" },
+                            requests: { cpu: "100m", memory: "128Mi" }
+                        }
+                    },
+                    adminUser: "admin",
+                    adminPassword: config.require("adminPassword"),
+                    plugins: [
+                        "grafana-piechart-panel",
+                        "camptocamp-prometheus-alertmanager-datasource",
+                        "grafana-oncall-app"
+                    ],
+                    datasources: {
+                        "datasources.yaml": {
+                            apiVersion: 1,
+                            datasources: [
+                                {
+                                    name: "DS_TEMPO",
+                                    type: "tempo",
+                                    access: "proxy",
+                                    url: "http://tempo-query-frontend.tracing.svc.cluster.local:3100",
+                                    version: 1
+                                },
+                                {
+                                    name: "DS_LOKI",
+                                    type: "loki",
+                                    access: "proxy",
+                                    url: "http://loki-query-frontend.logging.svc.cluster.local:3100",
+                                    version: 1,
+                                    jsonData: {
+                                        maxLines: 5000
+                                    },
+                                    derivedFields: {
+                                        datasourceUid: "",
+                                        matcherRegex: "(?:traceID|trace_id)=(\\w+)",
+                                        name: "TraceID",
+                                        url: "${__value.raw}"
+                                    }
+                                },
+                                {
+                                    name: "DS_PROMETHEUS",
+                                    type: "prometheus",
+                                    access: "proxy",
+                                    url: "http://thanos-query-frontend.monitoring.svc.cluster.local:9090",
+                                    version: 1
+                                },
+                                {
+                                    name: "DS_ALERTMANAGER",
+                                    type: "camptocamp-prometheus-alertmanager-datasource",
+                                    access: "proxy",
+                                    url: "http://kube-prometheus-stack-alertmanager.monitoring.svc.cluster.local:9093",
+                                    version: 1,
+                                    jsonData: {
+                                        severity_critical: "p1",
+                                        severity_high: "p2",
+                                        severity_warning: "p3",
+                                        severity_info: "p4"
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    "grafana.ini": {
+                        "auth.azuread": {
+                            name: "Azure AAD",
+                            enabled: true,
+                            allow_sign_up: true,
+                            client_id: "7d91f7eb-2dcb-4989-9b11-94bbf2322be5",
+                            client_secret: config.require("client_secret"),
+                            auth_url: "https://login.microsoftonline.com/e824e20c-c5d7-4a69-adb1-3494404763a5/oauth2/v2.0/authorize",
+                            token_url: "https://login.microsoftonline.com/e824e20c-c5d7-4a69-adb1-3494404763a5/oauth2/v2.0/token",
+                            role_attribute_strict: false
+                        },
+                        server: {
+                            root_url: "https://norther.example.com/grafana",
+                        },
+                        paths: {
+                            data: "/var/lib/grafana/",
+                            logs: "/var/log/grafana",
+                            plugins: "/var/lib/grafana/plugins",
+                            provisioning: "/etc/grafana/provisioning",
+                        },
+                        dataproxy: {
+                            timeout: "60",
+                            keep_alive_seconds: "60"
+                        },
+                        analytics: {
+                            check_for_updates: false,
+                            reporting_enabled: false
+                        },
+                        log: { mode: "console", level: "warn" },
+                        grafana_net: { url: "https://grafana.net" },
+                        user: {
+                            default_theme: "dark",
+                            home_page: ""
+                        },
+                        // tracing: { type: "jaeger" },
+                        // "tracing.jaeger": {
+                        //     address: "tempo-distributor.tracing.svc.cluster.local:6831",
+                        //     zipkin_propagation: true
+                        // }
+                    },
+                    sidecar: {
+                        resources: {
+                            limits: { cpu: "50m", memory: "128Mi" },
+                            requests: { cpu: "50m", memory: "128Mi" }
+                        },
+                        dashboards: { enabled: true, label: "grafana_dashboard" }
+                    }
                 }
             }
-        }
+        ]
     }
 ]
 
-for (var i in deploy_spec) {
-    // Create Kubernetes Namespace.
-    const namespace = new k8s.core.v1.Namespace(deploy_spec[i].namespace.metadata.name, {
-        metadata: deploy_spec[i].namespace.metadata,
-        spec: deploy_spec[i].namespace.spec
-    });
-    // Create Kubernetes ConfigMap.
-    for (var configmap_index in deploy_spec[i].configmap) {
-        const configmap = new k8s.core.v1.ConfigMap(deploy_spec[i].configmap[configmap_index].metadata.name, {
-            metadata: deploy_spec[i].configmap[configmap_index].metadata,
-            data: deploy_spec[i].configmap[configmap_index].data,
-        }, { dependsOn: [namespace] });
-    }
-    // Create Kubernetes Secret.
-    // for (var secret_index in deploy_spec[i].secret) {
-    //     const secret = new k8s.core.v1.Secret(deploy_spec[i].secret[secret_index].metadata.name, {
-    //         metadata: deploy_spec[i].secret[secret_index].metadata,
-    //         type: deploy_spec[i].secret[secret_index].type,
-    //         data: deploy_spec[i].secret[secret_index].data,
-    //         stringData: deploy_spec[i].secret[secret_index].stringData
-    //     }, { dependsOn: [namespace] });
-    // }
-    // Create Release Resource.
-    const release = new k8s.helm.v3.Release(deploy_spec[i].helm.name, {
-        namespace: deploy_spec[i].helm.namespace,
-        name: deploy_spec[i].helm.name,
-        chart: deploy_spec[i].helm.chart,
-        version: deploy_spec[i].helm.version,
-        values: deploy_spec[i].helm.values,
-        skipAwait: true,
-        repositoryOpts: {
-            repo: deploy_spec[i].helm.repository,
-        },
-    }, { dependsOn: [namespace] });
-}
+const namespace = new k8s_module.core.v1.Namespace('Namespace', { resources: resources })
+const secret = new k8s_module.core.v1.Secret('Secret', { resources: resources }, { dependsOn: [namespace] });
+const configmap = new k8s_module.core.v1.ConfigMap('ConfigMap', { resources: resources }, { dependsOn: [namespace] });
+const release = new k8s_module.helm.v3.Release('Release', { resources: resources }, { dependsOn: [namespace] });
--- vmware-exporter/norther/index.ts
+++ vmware-exporter/norther/index.ts
@@ -1,9 +1,9 @@
-import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
+import * as k8s_module from '../../../module/pulumi-ts-module-kubernetes';
 
 let config = new pulumi.Config();
 
-const deploy_spec = [
+const resources = [
     {
         namespace: {
             metadata: {
@@ -13,60 +13,42 @@ const deploy_spec = [
             },
             spec: {}
         },
-        helm: {
-            namespace: "vmware-exporter",
-            name: "vmware-exporter",
-            chart: "../../_chart/vmware-exporter.tgz",
-            repository: "",
-            version: "0.13.2",
-            values: {
-                replicaCount: 1,
-                vsphere: {
-                    user: "administrator@vsphere.local",
-                    password: config.require("password"),
-                    host: "vcenter.esxi.lab",
-                    collectors: {
-                        hosts: true,
-                        datastores: true,
-                        vms: true,
-                        vmguests: true,
-                        snapshots: false
+        release: [
+            {
+                namespace: "vmware-exporter",
+                name: "vmware-exporter",
+                chart: "../../_chart/vmware-exporter.tgz",
+                repository: "",
+                version: "0.13.2",
+                values: {
+                    replicaCount: 1,
+                    vsphere: {
+                        user: "administrator@vsphere.local",
+                        password: config.require("password"),
+                        host: "vcenter.esxi.lab",
+                        collectors: {
+                            hosts: true,
+                            datastores: true,
+                            vms: true,
+                            vmguests: true,
+                            snapshots: false
+                        }
+                    },
+                    image: { tag: "v0.18.4" },
+                    service: { enabled: true },
+                    resources: {
+                        limits: { cpu: "100m", memory: "128Mi" },
+                        requests: { cpu: "100m", memory: "128Mi" }
                     }
-                },
-                image: { tag: "v0.18.4" },
-                service: { enabled: true },
-                resources: {
-                    limits: { cpu: "100m", memory: "128Mi" },
-                    requests: { cpu: "100m", memory: "128Mi" }
                 }
             }
-        },
-        servicemonitor: [
-            { name: "./servicemonitor.yaml" },
+        ],
+        configfile: [
+            { file: "./servicemonitor.yaml" },
         ]
     }
 ]
 
-for (var i in deploy_spec) {
-    // Create Kubernetes Namespace.
-    const namespace = new k8s.core.v1.Namespace(deploy_spec[i].namespace.metadata.name, {
-        metadata: deploy_spec[i].namespace.metadata,
-        spec: deploy_spec[i].namespace.spec
-    });
-    // Create Release Resource.
-    const release = new k8s.helm.v3.Release(deploy_spec[i].helm.name, {
-        namespace: deploy_spec[i].helm.namespace,
-        name: deploy_spec[i].helm.name,
-        chart: deploy_spec[i].helm.chart,
-        version: deploy_spec[i].helm.version,
-        values: deploy_spec[i].helm.values,
-        skipAwait: true,
-    }, { dependsOn: [namespace] });
-    // Create service monitor.
-    for (var yaml_index in deploy_spec[i].servicemonitor) {
-        const guestbook = new k8s.yaml.ConfigFile(deploy_spec[i].servicemonitor[yaml_index].name, {
-            file: deploy_spec[i].servicemonitor[yaml_index].name,
-            skipAwait: true,
-        }, { dependsOn: [namespace] });
-    }
-}
+const namespace = new k8s_module.core.v1.Namespace('Namespace', { resources: resources })
+const release = new k8s_module.helm.v3.Release('Release', { resources: resources }, { dependsOn: [namespace] });
+const configfile = new k8s_module.yaml.ConfigFile('ConfigFile', { resources: resources }, { dependsOn: [release] });
