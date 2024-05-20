c3e83ace4299b4691f6adc669b28f111d7b43ee1
suzhetao <suzhetao@gmail.com>
2024-05-20 10:19:09 +0800

    modified:   vsphere-csi/README.md 	modified:   vsphere-csi/souther/index.ts 	deleted:    vsphere-csi/values.yaml 	modified:   vsphere-csi/vsphere-cpi.yaml

--- vsphere-csi/souther/index.ts
+++ vsphere-csi/souther/index.ts
@@ -1,51 +1,102 @@
-import * as k8s from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
+import * as k8s_module from '../../../../module/pulumi-ts-module-kubernetes';
 
 let config = new pulumi.Config();
 
-const deploy_spec = [
+const labels = {
+  customer: "demo",
+  environment: "dev",
+  project: "CSI",
+  group: "vSphere",
+  datacenter: "dc01",
+  domain: "local"
+}
+
+const resources = [
   {
-    helm: {
-      namespace: "kube-system",
-      name: "vsphere-cpi",
-      chart: "vsphere-cpi",
-      repository: "https://kubernetes.github.io/cloud-provider-vsphere",
-      version: "1.28.0",
-      values: {
-        config: {
-          enabled: true,
-          vcenter: "192.168.0.130",
-          username: "administrator@vsphere.local",
-          password: config.require("VSPHERE_PASSWORD"),
-          datacenter: "Demo"
+    namespace: {
+      metadata: {
+        name: "vmware-system-csi",
+        annotations: {},
+        labels: {}
+      },
+      spec: {}
+    },
+    secret: [
+      {
+        metadata: {
+          name: "vsphere-config-secret",
+          namespace: "vmware-system-csi",
+          annotations: {},
+          labels: {}
         },
-        podSecurityPolicy: { enabled: false },
-        daemonset: {
-          image: "registry.cn-shanghai.aliyuncs.com/goldenimage/mirrored-cloud-provider-vsphere-cpi-release-manager",
-          tag: "v1.28.0",
-          replicaCount: 1,
-          resources: {
-            limits: { cpu: "200m", memory: "256Mi" },
-            requests: { cpu: "200m", memory: "256Mi" }
+        type: "Opaque",
+        data: {
+          "csi-vsphere.conf": config.require("CSI-VSPHERE")
+        },
+        stringData: {}
+      }
+    ],
+    release: [
+      {
+        namespace: "kube-system",
+        name: "vsphere-cpi",
+        chart: "vsphere-cpi",
+        repositoryOpts: {
+          repo: "https://kubernetes.github.io/cloud-provider-vsphere"
+        },
+        version: "1.29.0",
+        values: {
+          config: {
+            enabled: true,
+            vcenter: "192.168.0.130",
+            username: "administrator@vsphere.local",
+            password: config.require("VSPHERE_PASSWORD"),
+            datacenter: "cn-north-1",
+            region: "k8s-region",
+            zone: "k8s-zone"
           },
-          podLabels: { customer: "demo", environment: "dev", project: "CPI", group: "vSphere", datacenter: "dc01", domain: "local" },
+          podSecurityPolicy: { enabled: false },
+          daemonset: {
+            image: "swr.cn-east-3.myhuaweicloud.com/cloud-provider-vsphere/manager",
+            tag: "v1.29.0",
+            replicaCount: 1,
+            resources: {
+              limits: { cpu: "50m", memory: "64Mi" },
+              requests: { cpu: "50m", memory: "64Mi" }
+            },
+            podLabels: labels
+          }
         }
       }
-    }
+    ],
+    storageclass: [
+      {
+        metadata: {
+          name: "vsphere-san-sc",
+          annotations: {
+            "storageclass.kubernetes.io/is-default-class": "true"
+          }
+        },
+        provisioner: "csi.vsphere.vmware.com",
+        allowVolumeExpansion: true,
+        parameters: {
+          datastoreurl: "ds:///vmfs/volumes/658f79a1-1ae1db3f-caaf-002590f4baa4/",
+          "csi.storage.k8s.io/fstype": "ext4"
+        },
+        reclaimPolicy: "Delete",
+        volumeBindingMode: "Immediate"
+
+      }
+    ],
+    configfile: [
+      { file: "../vsphere-csi-driver.yaml" }
+    ]
   }
 ]
 
-for (var i in deploy_spec) {
-  // Create Release Resource.
-  const release = new k8s.helm.v3.Release(deploy_spec[i].helm.name, {
-    namespace: deploy_spec[i].helm.namespace,
-    name: deploy_spec[i].helm.name,
-    chart: deploy_spec[i].helm.chart,
-    version: deploy_spec[i].helm.version,
-    values: deploy_spec[i].helm.values,
-    skipAwait: false,
-    repositoryOpts: {
-      repo: deploy_spec[i].helm.repository,
-    },
-  });
-}
+const namespace = new k8s_module.core.v1.Namespace('Namespace', { resources: resources })
+const secret = new k8s_module.core.v1.Secret('Secret', { resources: resources }, { dependsOn: [namespace] });
+const release = new k8s_module.helm.v3.Release('Release', { resources: resources });
+const configfile = new k8s_module.yaml.ConfigFile('ConfigFile', { resources: resources }, { dependsOn: [secret] });
+const storageclass = new k8s_module.storage.v1.StorageClass('StorageClass', { resources: resources }, { dependsOn: [configfile] });
